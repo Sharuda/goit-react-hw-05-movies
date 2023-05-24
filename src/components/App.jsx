693848bf0df7f77c10b818f07c5583d0c4d32239
Sharuda <sharuda.yana98@gmail.com>
2023-05-24 19:02:05 +0300

    add fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
+// import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+// const Layout = lazy(() =>
+//   import('./Layout').then(module => ({
+//     ...module,
+//     default: module.Layout,
+//   }))
+// );
+
+// const Movies = lazy(() =>
+//   import('../pages/Movies/Movies').then(module => ({
+//     ...module,
+//     default: module.Movies,
+//   }))
+// );
+// const MovieDetails = lazy(() =>
+//   import('../pages/MovieDetails/MovieDetails').then(module => ({
+//     ...module,
+//     default: module.MovieDetails,
+//   }))
+// );
+// const Home = lazy(() =>
+//   import('../pages/Home/Home').then(module => ({
+//     ...module,
+//     default: module.Home,
+//   }))
+// );
+// const Cast = lazy(() =>
+//   import('./Cast/Cast').then(module => ({
+//     ...module,
+//     default: module.Cast,
+//   }))
+// );
+// const Reviews = lazy(() =>
+//   import('./Review/Review').then(module => ({
+//     ...module,
+//     default: module.Reviews,
+//   }))
+// );
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        
+      </Routes>
+    </>
   );
 };
